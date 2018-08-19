900b6baccc86cc5367291caefc60ccdd34d80b7d
betterLeaf <18623742058@163.com>
2018-08-19 14:48:21 +0800

    v2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 class Person {
-    constructor(name) {
+    constructor(name, age) {
         this.name = name
+        this.age = age
     }
 
-    getName() {
-        return this.name
+    eat() {
+        alert(`${this.name} eat something`)
+    }
+    speak() {
+        alert(`My name is: ${this.name}, age: ${this.age}`)
     }
 }
 
-let p = new Person('betterLeaf');
+class Student extends Person {
+    constructor(name, age, number){
+        super(name, age)
+        this.number = number
+    }
 
-alert(p.getName())
+    study() {
+        alert(`${this.name} study`)
+    }
+}
+
+let xiaoming = new Student('xiaoming', 12, '001')
+xiaoming.study()
+xiaoming.speak()
+
+let xiaohong = new Student('xiaohong', 12, '002')
+xiaohong.study()
+xiaohong.speak()
