b2cc8a2f59ba030287e740af9700db802fc206b5
betterLeaf <18623742058@163.com>
2018-08-19 14:25:17 +0800

    init

---
+++ webpack.dev.config.js
@@ -0,0 +1,28 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname,
+        filename: './release/bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js?$/,
+            exclude: /(node_modules)/, //忽略此文件
+            loader: 'babel-loader'
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, './release'),
+        open: true,
+        port: 9000
+    }
+}
